d85333a41dcf6bbd277402abc668911a44257267
Víťa Plšek <vita@angular.cz>
2016-01-27 22:41:20 +0100

    upgrade of phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/phantomjs-polyfill/bind-polyfill.js',
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'node_modules/angular-ui-router/release/angular-ui-router.js',
